5a6fa8d305aa622452e7fb25a8a17bebe50a35eb
SMYK <samyak.sage@gmail.com>
2026-01-24 22:56:49 +0530

    Add file 2.cpp

---
+++ Assignment2/2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N; 
+    for (int i = 1; i <= N; i++) {
+      
+        for (int j = 1; j <= N - i; j++) {
+            cout << "\t";  
+        }
+
+        for (int j = i; j <= 2*i - 1; j++) {
+            cout << j << "\t";  
+        }
+
+    
+        for (int j = 2*i - 2; j >= i; j--) {
+            cout << j << "\t";  
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
