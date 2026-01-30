7b7db1e312cf0300d4fae699b58b124a9dc0acb6
SMYK <samyak.sage@gmail.com>
2026-01-30 21:32:57 +0530

    Add file 5.cpp

---
+++ Attempt/5.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+
+bool isPrime(int n) {
+        if (n<=1){ return false;}
+        for (int i = 2; i*i <= n; i++){
+            if (n%i==0) return false;
+        }
+        return true;
+    }
+
+int main(){
+    int m;
+    cin>>m;
+
+    int n = m/2+1;
+    
+    int prime = 2;
+
+    for(int i = n ; i >= 1 ; i--){
+        for (int j = 1 ; j <= m-i  ; j++){
+           cout << "  ";
+        }
+        if(i==2){
+        for (int j = 1 ; j <= 2*i-2 ; j++){
+            while (!isPrime(prime)){
+                prime++;
+            }
+            cout << prime << " ";
+            prime++;
+        }
+    }
+        else{
+        for (int j = 1 ; j <= 2*i-1 ; j++){
+            while (!isPrime(prime)){
+                prime++;
+            }
+            cout << prime << " ";
+            prime++;
+        }
+    }
+        cout << endl;
+    }
+    
+    prime = 2;
+    
+    for(int i = 2; i <= n; i++){
+        for (int j = 1 ; j <= m-i  ; j++){
+           cout << "  ";
+        }
+        if(i==n-1){
+        for (int j = 1 ; j <= 2*i-2 ; j++){
+            while (!isPrime(prime)){
+                prime++;
+            }
+            cout << prime << " ";
+            prime++;
+        }
+    }
+        else{
+        for (int j = 1 ; j <= 2*i-1 ; j++){
+            while (!isPrime(prime)){
+                prime++;
+            }
+            cout << prime << " ";
+            prime++;
+        }
+    }
+        cout << endl;
+    }
+
+}
